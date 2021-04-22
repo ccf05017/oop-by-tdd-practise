15e22cd5b09d5977b1393e5c7befd021a36f2cde
poppo <poppo@woowahan.com>
2021-04-22 09:27:48 +0900

    project init

---
+++ src/main/java/com/poppo/oopbytddpractise/OopByTddPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.poppo.oopbytddpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopByTddPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopByTddPractiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poppo/oopbytddpractise/OopByTddPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poppo.oopbytddpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopByTddPractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
